622aadc5926604ad52c0e0ff3dd805e450ad559e
daffaromero <70407002+daffaromero@users.noreply.github.com>
2021-05-03 22:24:22 +0700

    Add files via upload

---
+++ Database/Tables/dbo.Course.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Course] (
+    [CourseName]   NVARCHAR (450) NOT NULL,
+    [CourseLevel]  NVARCHAR (30)  NOT NULL,
+    [CourseRating] NVARCHAR (30)  NULL,
+    CONSTRAINT [PK_Course] PRIMARY KEY CLUSTERED ([CourseName] ASC)
+);
+
